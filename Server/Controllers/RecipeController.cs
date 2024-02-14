3083a48f2a06fc0a2669ea6c7202d8c071cabaae
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2024-02-14 12:37:41 -0500

    RecipeController created

---
+++ Server/Controllers/RecipeController.cs
@@ -0,0 +1,17 @@
+﻿using AIChef.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AIChef.Server.Controllers
+{
+	[Route("[controller]")]
+	[ApiController]
+	public class RecipeController : ControllerBase
+	{
+		[HttpPost, Route("GetRecipeIdeas")]
+		public async Task<ActionResult<List<Idea>>> GetRecipeIdeas(RecipeParms recipeParms) 
+		{
+			return SampleData.RecipeIdeas;
+		}
+	}
+}
