dc4ab8baa302611460bf926caf7b34d0b02c4c95
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2024-02-15 02:23:32 -0500

    OpenAI service created, tested, and is working correctly

--- Server/Controllers/RecipeController.cs
+++ Server/Controllers/RecipeController.cs
@@ -1,4 +1,5 @@
-﻿using AIChef.Shared;
+﻿using AIChef.Server.Services.Interfaces;
+using AIChef.Shared;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,10 +9,31 @@ namespace AIChef.Server.Controllers
 	[ApiController]
 	public class RecipeController : ControllerBase
 	{
-		[HttpPost, Route("GetRecipeIdeas")]
+		private readonly IOpenAIApi _openAiService;
+
+        public RecipeController(IOpenAIApi openAiService)
+        {
+            _openAiService = openAiService;
+        }
+
+        [HttpPost, Route("GetRecipeIdeas")]
 		public async Task<ActionResult<List<Idea>>> GetRecipeIdeas(RecipeParms recipeParms) 
 		{
-			return SampleData.RecipeIdeas;
+			//return SampleData.RecipeIdeas;
+			string mealTime = recipeParms.MealTime;
+			List<string> ingredients = recipeParms.Ingredients
+												  .Where(i => !string.IsNullOrEmpty(i.Description))
+												  .Select(i => i.Description!)
+												  .ToList();
+
+			if (string.IsNullOrEmpty(mealTime))
+			{
+				mealTime = "Breakfast";
+			}
+
+			var ideas = await _openAiService.CreateRecipeIdeas(mealTime, ingredients);
+			return ideas;
+
 		}
 	}
 }
---
+++ Server/Services/Interfaces/IOpenAIApi.cs
@@ -0,0 +1,11 @@
+﻿using AIChef.Shared;
+
+namespace AIChef.Server.Services.Interfaces
+{
+	public interface IOpenAIApi
+	{
+		Task<List<Idea>> CreateRecipeIdeas(string mealTime, List<string> ingredients);
+
+
+	}
+}
